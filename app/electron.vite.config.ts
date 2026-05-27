9adac9554151e1dd58887ba55c5297a7cdb35a58
Coleman Irby <ckirbyee92@gmail.com>
2026-05-27 15:24:25 -0500

    Remove externalize all deps

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -3,18 +3,6 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {
-    build: {
-      externalizeDeps: {
-        exclude: [
-          '@electron-toolkit/utils',
-          'mime-types',
-          'dotenv',
-          'mime',
-        ]
-      }
-    },
-  },
   renderer: {
     resolve: {
       alias: {

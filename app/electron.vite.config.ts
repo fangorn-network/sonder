2b450e6a9cf40a9a531d21ebaaa0874942eef9d8
Coleman Irby <ckirbyee92@gmail.com>
2026-05-27 15:37:58 -0500

    Added back preload and main

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -3,6 +3,20 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  main: {
+    build: {
+      rollupOptions: {
+        external: ['electron']
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        external: ['electron']
+      }
+    }
+  },
   renderer: {
     resolve: {
       alias: {

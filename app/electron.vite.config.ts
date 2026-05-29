9e158eda3c300eaa4ef22e63338eb9c935e8723d
Coleman Irby <ckirbyee92@gmail.com>
2026-05-29 10:48:42 -0500

    Remove dbus-native from exclude

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -3,13 +3,7 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {
-    build: {
-      rollupOptions: {
-        external: ['dbus-next']
-      }
-    }
-  },
+  main: {},
   preload: {},
   renderer: {
       build: {
@@ -28,7 +22,7 @@ export default defineConfig({
     },
     plugins: [react()],
     server: {
-      proxy: {
+      proxy: { 
         '/search': 'http://localhost:8080',
         '/health': 'http://localhost:8080',
         '/reingest': 'http://localhost:8080',

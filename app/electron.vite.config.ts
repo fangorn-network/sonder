2b63028c5a7393dbee2f6c0b31c299186066f5c5
Coleman Irby <ckirbyee92@gmail.com>
2026-05-29 11:23:40 -0500

    modify config again

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -3,13 +3,7 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {
-    build: {
-      rollupOptions: {
-        external: ['dbus-next']
-      }
-    }
-  },
+  main: {},
   preload: {},
   renderer: {
       build: {
